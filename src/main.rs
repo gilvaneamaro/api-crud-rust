ab7cbf1ae978474b79ece23b2b083bde3b73a4d8
Gilvane Amaro <gilvane.amaro.jr@gmail.com>
2024-07-16 21:00:18 -0300

    Create connection database, model

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-use std::io;
-
 fn main() {
-    println!("Hello,{0}! Thats your first code in Rust! Lets change to {1}", "Gilvane Amaro", "Ruby?");
-
-    let mut input = String::new();
-
-    println!("Lets play a game, enter with the word:");
-
-    io::stdin().read_line(&mut input).expect("Falha ao ler a linha");
-    let input = input.trim();
-
-    for i in 1..5 {
-        println!("i: {}", i);
-    }
-    println!("Sua palavra foi {}", input);
 }
 
